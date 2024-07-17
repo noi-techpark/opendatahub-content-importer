d9307e7383c10541e00de098f8c5d08a945876bd
RudiThoeni <r.thoeni@noi.bz.it>
2024-07-17 11:13:25 +0200

    wip accommodation transformer

---
+++ AccommodationTransformer/Parser/AccommodationParser.cs
@@ -0,0 +1,58 @@
+﻿using DataModel;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace AccommodationTransformer.Parser
+{
+    public class AccommodationParser
+    {
+        public static AccommodationLinked ParseLTSAccommodation(JObject accomodationdetail, bool reduced)
+        {
+            AccommodationLinked accommodationlinked = new AccommodationLinked();
+            
+            accommodationlinked.Id = accomodationdetail["rid"].Value<string>();
+            accommodationlinked._Meta = new Metadata() { Id = accommodationlinked.Id, LastUpdate = DateTime.Now, Reduced = reduced, Source = "lts", Type = "accommodation", UpdateInfo = new UpdateInfo() { UpdatedBy = "importer.v2", UpdateSource = "lts.interface.v2" } };           
+
+            //Accommodation Type
+
+            //Accommodation Category
+
+            //Accommodation Detail
+
+            //Address Groups
+
+            //Amenities
+
+            //GPS Info
+
+            //Images
+
+            //Galleries
+
+            //District
+
+
+
+            //TODO PARSE ACCOMMODATION
+            var name = accomodationdetail["contacts"].Value<JArray>().FirstOrDefault()["address"]["name"].Value<JObject>();
+            string namede = "";
+
+            if (name != null)
+            {
+                JToken token = name["de"];
+                if (token != null)
+                {
+                    namede = token.Value<string>();
+                }
+            }
+
+
+            return accommodationlinked;
+        }
+    }
+}
--- AccommodationTransformer/RabbitMQService.cs
+++ AccommodationTransformer/RabbitMQService.cs
@@ -19,94 +19,11 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using TransformerHelper;
 
 namespace AccommodationTransformer
 {
-    #region Generic Code
-    public interface IReadMessage
-    {
-        void Read(string connectionstring, string mongoconnection, List<string> queues, IDictionary<string, DataImport> dataiport);
-    }
-
-    public abstract class ReadMessage : IReadMessage
-    {
-        protected string mongodbconnection;
-        protected string rabbitmqconnection;
-        protected IDictionary<string, DataImport> dataimport;
-
-        public void Read(string connectionstring, string mongoconnection, List<string> queues, IDictionary<string, DataImport> _dataimport)
-        {
-            rabbitmqconnection = connectionstring;
-            var _rabbitMQServer = new ConnectionFactory() { Uri = new Uri(connectionstring) };
-
-            using var connection = _rabbitMQServer.CreateConnection();
-
-            using var channel = connection.CreateModel();
-
-            mongodbconnection = mongoconnection;
-            dataimport = _dataimport;
-
-            StartReading(channel, queues);
-        }
-
-        private async void StartReading(IModel channel, List<string> queues)
-        {
-            foreach (var queueName in queues)
-            {
-                // connect to the queue
-                channel.QueueDeclare(queueName,
-                    durable: true,
-                    exclusive: false,
-                    autoDelete: false,
-                    arguments: null);
-
-                // Consumer definition
-                var consumer = new EventingBasicConsumer(channel);
-
-                // Definition of event when the Consumer gets a message
-                consumer.Received += async (sender, e) =>
-                {
-                    var result = await ManageMessage(e);
-                    if(result)
-                        channel.BasicAck(e.DeliveryTag, false);
-                };
-
-                // Start pushing messages to our consumer
-                channel.BasicConsume(queueName, false, consumer);
-
-                Console.WriteLine("Consumer is running");
-            }
-            Console.ReadLine();
-        }
-
-        private async Task<bool> ManageMessage(BasicDeliverEventArgs e)
-        {
-            var body = e.Body.ToArray();
-            var message = Encoding.UTF8.GetString(body);
-            Console.WriteLine("received: " + message);
-
-            var rabbitmessage = JsonConvert.DeserializeObject<RabbitNotifyMessage>(message);
-
-            //Load from MongoDB
-            var data = await LoadDataFromMongo(rabbitmessage);
-
-            return await TransformData(data, e.RoutingKey);            
-        }
-
-        private async Task<MongoDBObject> LoadDataFromMongo(RabbitNotifyMessage message)
-        {
-            MongoDBReader mongoreader = new MongoDBReader(mongodbconnection);
-            return await mongoreader.GetFromMongoAsObject(message);
-        }
-
-        public virtual async Task<bool> TransformData(MongoDBObject data, string routingkey)
-        {
-            //Transformer Logic goes here
-            throw new NotImplementedException();
-        }
-    }
-
-    #endregion
+    
 
 
     public interface IReadAccommodation : IReadMessage
@@ -147,6 +64,7 @@ namespace AccommodationTransformer
             {
                 Console.WriteLine("Read Accommodation DETAIL called");
 
+               
                 JObject accomodationdetail = jsonarray.FirstOrDefault()["data"].Value<JObject>();
 
                 //TODO PARSE ACCOMMODATION
