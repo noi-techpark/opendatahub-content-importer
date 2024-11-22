70b0cc95f848f9f1794bfc0fa15f91e7134d5a55
RudiThoeni <r.thoeni@noi.bz.it>
2024-11-22 09:56:40 +0100

    wip accommodationsearch parsing

--- LTSAPI/Parser/AccommodationSearchResultParser.cs
+++ LTSAPI/Parser/AccommodationSearchResultParser.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,7 +30,162 @@ namespace LTSAPI.Parser
             LTSAvailabilitySearchResult accommodationsearchresult,
             int rooms)
         {
-            return null;
+            try
+            {
+                if (accommodationsearchresult != null)
+                {
+                  
+                    CultureInfo culturede = CultureInfo.CreateSpecificCulture("de");
+
+                    MssResult result = new MssResult();
+                    result.ResultId = accommodationsearchresult.resultSet.rid;
+                    //min amount max amount
+                    result.bookableHotels = accommodationsearchresult.data != null ? accommodationsearchresult.data.Count() : 0;
+                    result.CheapestChannel = "lts";
+                    result.Cheapestprice = 0;
+
+                    if (accommodationsearchresult.data != null)
+                    {
+                        foreach (var offerdetail in accommodationsearchresult.data)
+                        {
+                            MssResponseShort lcsresponseshort = new MssResponseShort();
+
+                            lcsresponseshort.A0RID = offerdetail.accommodation.rid;
+                            lcsresponseshort.HotelId = 0;
+                            lcsresponseshort.ChannelID = "lts";
+
+                            //Check Günstigstes Angebot
+                            List<CheapestOffer> CheapestOffer_ws = new List<CheapestOffer>();
+                            List<CheapestOffer> CheapestOffer_bb = new List<CheapestOffer>();
+                            List<CheapestOffer> CheapestOffer_hb = new List<CheapestOffer>();
+                            List<CheapestOffer> CheapestOffer_fb = new List<CheapestOffer>();
+                            List<CheapestOffer> CheapestOffer_ai = new List<CheapestOffer>();
+
+                            //Für jedes Zimmerangebot
+                            foreach (var roomdetail in offerdetail.roomOptions)
+                            {
+                                foreach (var roomdetailoffer in roomdetail.rates)
+                                {
+                                    RoomDetails myroomdetail = new RoomDetails();
+
+                                    myroomdetail.RoomSeq = roomdetailoffer.roomOptionId;
+                                    myroomdetail.RoomId = roomdetail.roomGroup.rid;
+                                    myroomdetail.OfferId = roomdetailoffer.ratePlan.rid; // TODO change to rommdetailoffer.RID
+                                    myroomdetail.Price_ai = roomdetailoffer.allInclusiveAmount;
+                                    myroomdetail.Price_bb = roomdetailoffer.bedAndBreakfastAmount;
+                                    myroomdetail.Price_fb = roomdetailoffer.fullBoardAmount;
+                                    myroomdetail.Price_hb = roomdetailoffer.halfBoardAmount;
+                                    myroomdetail.Price_ws = roomdetailoffer.roomOnlyAmount;
+                                    myroomdetail.TotalPrice = 0;
+                                    myroomdetail.TotalPriceString = "";
+                                    //myroomdetail.Roomtitle = roomdetail.Name;  //available with more details??
+                                    //myroomdetail.Roomdesc = roomdetailoffer.
+                                    myroomdetail.Roomfree = roomdetail.availableRooms;
+                                    myroomdetail.Roommax = 0;
+                                    myroomdetail.Roommin = 0;
+                                    myroomdetail.Roomstd = 0;
+                                    //NOT found the roomtype?
+                                    //myroomdetail.Roomtype = roomdetail.Genre != null ? Convert.ToInt32(roomdetail.Genre) : 0;  //roomdetail.Genre;
+                                                                                                                               //myroomdetail.Roomtype = 0;
+
+                                    if (roomdetailoffer.allInclusiveAmount != 0)
+                                    {
+                                        var mycheapestofferai = new CheapestOffer() { RoomId = roomdetail.roomGroup.rid, Price = roomdetailoffer.allInclusiveAmount ?? 0, RoomSeq = (int)roomdetailoffer.roomOptionId, RoomFree = roomdetail.availableRooms };
+                                        CheapestOffer_ai.Add(mycheapestofferai);
+                                    }
+                                    if (roomdetailoffer.bedAndBreakfastAmount != 0)
+                                    {
+                                        var mycheapestofferbb = new CheapestOffer() { RoomId = roomdetail.roomGroup.rid, Price = roomdetailoffer.bedAndBreakfastAmount ?? 0, RoomSeq = (int)roomdetailoffer.roomOptionId, RoomFree = roomdetail.availableRooms };
+                                        CheapestOffer_bb.Add(mycheapestofferbb);
+                                    }
+                                    if (roomdetailoffer.halfBoardAmount != 0)
+                                    {
+                                        var mycheapestofferhb = new CheapestOffer() { RoomId = roomdetail.roomGroup.rid, Price = roomdetailoffer.halfBoardAmount ?? 0, RoomSeq = (int)roomdetailoffer.roomOptionId, RoomFree = roomdetail.availableRooms };
+                                        CheapestOffer_hb.Add(mycheapestofferhb);
+                                    }
+                                    if (roomdetailoffer.fullBoardAmount != 0)
+                                    {
+                                        var mycheapestofferfb = new CheapestOffer() { RoomId = roomdetail.roomGroup.rid, Price = roomdetailoffer.fullBoardAmount ?? 0, RoomSeq = (int)roomdetailoffer.roomOptionId, RoomFree = roomdetail.availableRooms };
+                                        CheapestOffer_fb.Add(mycheapestofferfb);
+                                    }
+                                    if (roomdetailoffer.roomOnlyAmount != 0)
+                                    {
+                                        var mycheapestofferws = new CheapestOffer() { RoomId = roomdetail.roomGroup.rid, Price = roomdetailoffer.roomOnlyAmount ?? 0, RoomSeq = (int)roomdetailoffer.roomOptionId, RoomFree = roomdetail.availableRooms };
+                                        CheapestOffer_ws.Add(mycheapestofferws);
+                                    }
+
+
+                                    lcsresponseshort.RoomDetails.Add(myroomdetail);
+                                }
+                            }
+
+
+                            //Getting cheapest offer
+                            var cheapestofferobj_ai = RoomCalculationHelper.CalculateCheapestRooms(CheapestOffer_ai, rooms, "ai");
+                            lcsresponseshort.CheapestOffer_ai = cheapestofferobj_ai != null ? cheapestofferobj_ai.Price : 0;
+                            if (cheapestofferobj_ai != null && cheapestofferobj_ai.Price > 0)
+                                lcsresponseshort.CheapestOfferDetail.Add(cheapestofferobj_ai);
+
+                            var cheapestofferobj_bb = RoomCalculationHelper.CalculateCheapestRooms(CheapestOffer_bb, rooms, "bb");
+                            lcsresponseshort.CheapestOffer_bb = cheapestofferobj_bb != null ? cheapestofferobj_bb.Price : 0;
+                            if (cheapestofferobj_bb != null && cheapestofferobj_bb.Price > 0)
+                                lcsresponseshort.CheapestOfferDetail.Add(cheapestofferobj_bb);
+
+                            var cheapestofferobj_hb = RoomCalculationHelper.CalculateCheapestRooms(CheapestOffer_hb, rooms, "hb");
+                            lcsresponseshort.CheapestOffer_hb = cheapestofferobj_hb != null ? cheapestofferobj_hb.Price : 0;
+                            if (cheapestofferobj_hb != null && cheapestofferobj_hb.Price > 0)
+                                lcsresponseshort.CheapestOfferDetail.Add(cheapestofferobj_hb);
+
+                            var cheapestofferobj_fb = RoomCalculationHelper.CalculateCheapestRooms(CheapestOffer_fb, rooms, "fb");
+                            lcsresponseshort.CheapestOffer_fb = cheapestofferobj_fb != null ? cheapestofferobj_fb.Price : 0;
+                            if (cheapestofferobj_fb != null && cheapestofferobj_fb.Price > 0)
+                                lcsresponseshort.CheapestOfferDetail.Add(cheapestofferobj_fb);
+
+                            var cheapestofferobj_ws = RoomCalculationHelper.CalculateCheapestRooms(CheapestOffer_ws, rooms, "ws");
+                            lcsresponseshort.CheapestOffer_ws = cheapestofferobj_ws != null ? cheapestofferobj_ws.Price : 0;
+                            if (cheapestofferobj_ws != null && cheapestofferobj_ws.Price > 0)
+                                lcsresponseshort.CheapestOfferDetail.Add(cheapestofferobj_ws);
+
+                            //Cheapest Offer General
+                            if (lcsresponseshort.CheapestOffer == 0)
+                            {
+                                List<double> cheapestoffertotal = new List<double>();
+                                if (lcsresponseshort.CheapestOffer_ai > 0)
+                                    cheapestoffertotal.Add((double)lcsresponseshort.CheapestOffer_ai);
+                                if (lcsresponseshort.CheapestOffer_bb > 0)
+                                    cheapestoffertotal.Add((double)lcsresponseshort.CheapestOffer_bb);
+                                if (lcsresponseshort.CheapestOffer_fb > 0)
+                                    cheapestoffertotal.Add((double)lcsresponseshort.CheapestOffer_fb);
+                                if (lcsresponseshort.CheapestOffer_hb > 0)
+                                    cheapestoffertotal.Add((double)lcsresponseshort.CheapestOffer_hb);
+                                if (lcsresponseshort.CheapestOffer_ws > 0)
+                                    cheapestoffertotal.Add((double)lcsresponseshort.CheapestOffer_ws);
+
+                                if (cheapestoffertotal.Count > 0)
+                                {
+                                    var cheapestofferdouble = cheapestoffertotal.OrderBy(x => x).FirstOrDefault();
+                                    lcsresponseshort.CheapestOffer = cheapestofferdouble;
+                                    lcsresponseshort.CheapestOfferString = String.Format(culturede, "{0:0,0.00}", cheapestofferdouble);
+                                }
+
+                            }
+
+                            //Add only if there is a valid Offer (enough Roomfree etc..)
+                            if (lcsresponseshort.CheapestOfferDetail != null && lcsresponseshort.CheapestOfferDetail.Count > 0)
+                                result.MssResponseShort.Add(lcsresponseshort);
+                        }
+                    }
+
+
+                    return result;
+                }
+                else
+                    return null;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
 
     }
---
+++ LTSAPI/Parser/RoomCalculationHelper.cs
@@ -0,0 +1,210 @@
+﻿using Combinatorics.Collections;
+using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTSAPI
+{
+    public class RoomCalculationHelper
+    {
+        public static CheapestRoomCombination CalculateCheapestRooms(IEnumerable<CheapestOffer> cheapestofferlist, int rooms, string service)
+        {
+            if (cheapestofferlist != null && cheapestofferlist.Count() > 0 && cheapestofferlist.Count() >= rooms)
+            {
+                if (rooms == 1)
+                {
+                    var cheapestorffersingle = cheapestofferlist.OrderBy(x => x.Price).Take(1).FirstOrDefault();
+                    CheapestRoomCombination cheapestroomcombinationresult = new CheapestRoomCombination();
+                    if (cheapestorffersingle is { })
+                        cheapestroomcombinationresult.CheapestRoomCombinationDetail = new List<CheapestOffer>() { cheapestorffersingle };
+                    cheapestroomcombinationresult.Service = service;
+
+                    return cheapestroomcombinationresult;
+                }
+                else
+                    return CalculateCheapestRoomCombinations(cheapestofferlist, rooms, service);
+                //else
+                //{
+                //    //count if all romseqs are in the cheapestofferlist
+                //    var romseqspresent = cheapestofferlist.Select(x => x.RoomSeq).Distinct().Count();
+
+                //    if (romseqspresent == rooms)
+                //        return CalculateCheapestRoomCombinationsTemp(cheapestofferlist, rooms, service);
+                //    else
+                //        return new CheapestRoomCombination() { Service = service, CheapestRoomCombinationDetail = new List<CheapestOffer>() };
+                //}
+            }
+            else
+            {
+                return new CheapestRoomCombination() { Service = service, CheapestRoomCombinationDetail = new List<CheapestOffer>() };
+            }
+        }
+
+        private static CheapestRoomCombination CalculateCheapestRoomCombinationsWithCombinations(IEnumerable<CheapestOffer> cheapestofferlist, int rooms, string service)
+        {
+            List<CheapestRoomCombination> mycombinationresult = new List<CheapestRoomCombination>();
+
+            //Create combinations TO verify use variations??
+            Combinations<CheapestOffer> combinations = new Combinations<CheapestOffer>(cheapestofferlist, rooms);
+
+            foreach (IList<CheapestOffer> c in combinations)
+            {
+                bool addcombination = true;
+
+                //Check with roomseqdict if the combination is valid
+                for (int i = 0; i < rooms; i++)
+                {
+                    if (c.Where(x => x.RoomSeq == c[i].RoomSeq).Count() > 1)
+                    {
+                        //Removing combination - room in the same roomseq
+                        addcombination = false;
+                    }
+
+                    if (!addcombination)
+                        break;
+                }
+
+                //Remove all combinations where more rooms are used than roomfree                 
+                if (addcombination)
+                {
+                    for (int i = 0; i < rooms; i++)
+                    {
+                        //Check how often room with this id is used
+                        if (c.Where(x => x.RoomId == c[i].RoomId).Count() > c[i].RoomFree)
+                        {
+                            //Removing combination - room used more than available
+                            addcombination = false;
+                        }
+
+                        if (!addcombination)
+                            break;
+                    }
+                }
+
+                if (addcombination)
+                {
+                    mycombinationresult.Add(new CheapestRoomCombination() { CheapestRoomCombinationDetail = c, Service = service });
+                }
+            }
+
+            //Return Cheapestroomcombination
+            var cheapestcombination = mycombinationresult.OrderBy(x => x.Price).Take(1).FirstOrDefault();
+
+            return cheapestcombination ?? new();
+        }
+
+
+        //TO TEST
+        private static CheapestRoomCombination CalculateCheapestRoomCombinations(IEnumerable<CheapestOffer> cheapestofferlist, int rooms, string service)
+        {
+            //Create an Array with a Collection
+            List<CheapestOffer>[] offerbyroomseq = new List<CheapestOffer>[rooms];
+
+            CheapestRoomCombination cheapestroomcombination = new CheapestRoomCombination();
+            cheapestroomcombination.Service = service;
+            cheapestroomcombination.CheapestRoomCombinationDetail = new List<CheapestOffer>();
+
+            for (int i = 1; i <= rooms; i++)
+            {
+                //To check could it be that a roomseq does not exist?
+                offerbyroomseq[i - 1] = cheapestofferlist.Where(x => x.RoomSeq == i).OrderBy(x => x.Price).ToList();
+            }
+
+            //TO check, are there offerbyroomseq with not enough rooms?
+            if (offerbyroomseq.Count() < rooms)
+                throw new Exception("there is something not working");
+
+            foreach (var roomseq in offerbyroomseq.OrderBy(x => x.Count))
+            {
+                foreach (var room in roomseq)
+                {
+                    //roomfre
+                    int? roomfree = room.RoomFree;
+                    //Count total rooms
+
+                    //Count used rooms in cheapestroomcombination
+                    var usedroomscount = 0;
+                    if (cheapestroomcombination.CheapestRoomCombinationDetail.Count > 0)
+                    {
+                        usedroomscount = cheapestroomcombination.CheapestRoomCombinationDetail.Count(x => x.RoomId == room.RoomId);
+                    }
+
+                    //if roomfree > cheapestroomcombination
+                    if (roomfree > usedroomscount)
+                    {
+                        cheapestroomcombination.CheapestRoomCombinationDetail.Add(room);
+                        break;
+                    }
+                }
+            }
+
+            //If there are cheapestroomcombinations with not enough rooms return null
+            if (cheapestroomcombination.CheapestRoomCombinationDetail.Count < rooms)
+                return new(); //throw new Exception("there is something not working");
+
+            return cheapestroomcombination;
+        }
+    }
+
+    public static class CombinationsGeneric
+    {
+        private static void InitIndexes(int[] indexes)
+        {
+            for (int i = 0; i < indexes.Length; i++)
+            {
+                indexes[i] = i;
+            }
+        }
+
+        private static void SetIndexes(int[] indexes, int lastIndex, int count)
+        {
+            indexes[lastIndex]++;
+            if (lastIndex > 0 && indexes[lastIndex] == count)
+            {
+                SetIndexes(indexes, lastIndex - 1, count - 1);
+                indexes[lastIndex] = indexes[lastIndex - 1] + 1;
+            }
+        }
+
+        private static List<T> TakeAt<T>(int[] indexes, IEnumerable<T> list)
+        {
+            List<T> selected = new List<T>();
+            for (int i = 0; i < indexes.Length; i++)
+            {
+                selected.Add(list.ElementAt(indexes[i]));
+            }
+            return selected;
+        }
+
+        private static bool AllPlacesChecked(int[] indexes, int places)
+        {
+            for (int i = indexes.Length - 1; i >= 0; i--)
+            {
+                if (indexes[i] != places)
+                    return false;
+                places--;
+            }
+            return true;
+        }
+
+        public static IEnumerable<List<T>> GetDifferentCombinations<T>(this IEnumerable<T> collection, int count)
+        {
+            int[] indexes = new int[count];
+            int listCount = collection.Count();
+            if (count > listCount)
+                throw new InvalidOperationException($"{nameof(count)} is greater than the collection elements.");
+            InitIndexes(indexes);
+            do
+            {
+                var selected = TakeAt(indexes, collection);
+                yield return selected;
+                SetIndexes(indexes, indexes.Length - 1, listCount);
+            }
+            while (!AllPlacesChecked(indexes, listCount));
+
+        }
+    }
+}
